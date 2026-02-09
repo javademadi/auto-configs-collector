dcc6cb700d344fea31534552cf1b991c4e5ce9b0
Javad Emadi <82391127+javademadi@users.noreply.github.com>
2026-02-09 11:22:33 +0330

    Update qrcode_gen.py

--- collector/qrcode_gen.py
+++ collector/qrcode_gen.py
@@ -1,5 +1,13 @@
 import qrcode
 
-def make_qr(text, path):
-    img = qrcode.make(text)
-    img.save(path)
+def make_qr(text: str, out: str):
+    qr = qrcode.QRCode(
+        version=None,  # auto
+        error_correction=qrcode.constants.ERROR_CORRECT_Q,
+        box_size=8,
+        border=2,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(out)
