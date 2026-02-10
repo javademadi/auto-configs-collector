d6e001d4c0a348cacc536c9ebaacc1f984ceb1ba
Javad Emadi <82391127+javademadi@users.noreply.github.com>
2026-02-10 10:41:51 +0330

    Update alive_filter.py

--- collector/alive_filter.py
+++ collector/alive_filter.py
@@ -1,19 +1,36 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from collector.parser import is_valid_structure
 from collector.alive import is_alive_network
 
 
-def filter_alive_configs(configs, timeout=1.5):
+def _check_one(config, timeout):
+    # مرحله ۱: تست ساختاری
+    if not is_valid_structure(config):
+        return None
+
+    # مرحله ۲: تست شبکه
+    if not is_alive_network(config, timeout):
+        return None
+
+    return config
+
+
+def filter_alive_configs(
+    configs,
+    timeout=1.2,
+    max_workers=20
+):
     alive = []
 
-    for c in configs:
-        # مرحله ۱: تست ساختاری
-        if not is_valid_structure(c):
-            continue
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [
+            executor.submit(_check_one, c, timeout)
+            for c in configs
+        ]
 
-        # مرحله ۲: تست شبکه
-        if not is_alive_network(c, timeout=timeout):
-            continue
-
-        alive.append(c)
+        for future in as_completed(futures):
+            result = future.result()
+            if result:
+                alive.append(result)
 
     return alive
