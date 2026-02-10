18bdef4e9b1683bc8fb0bdf2dab82990b8e8ca07
Javad Emadi <82391127+javademadi@users.noreply.github.com>
2026-02-10 10:41:32 +0330

    Update alive.py

--- collector/alive.py
+++ collector/alive.py
@@ -2,20 +2,17 @@ import socket
 from urllib.parse import urlparse
 
 
-def is_alive_network(config: str, timeout=1.5) -> bool:
+def is_alive_network(config: str, timeout=1.2) -> bool:
     try:
         url = urlparse(config)
-
         host = url.hostname
         port = url.port
 
         if not host or not port:
             return False
 
-        # DNS resolve
         socket.gethostbyname(host)
 
-        # TCP check
         with socket.create_connection((host, port), timeout=timeout):
             return True
 
